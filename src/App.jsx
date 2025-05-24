bdde5d9cee840e878e00d5f5d73a67abb568561a
Lia <115329115+LiaWritesCode@users.noreply.github.com>
2025-05-24 02:58:09 -0400

    another attempt at push

--- src/App.jsx
+++ src/App.jsx
@@ -42,24 +42,20 @@ const App = () => {
 
       {/* Cart Icon with Total Quantity */}
 <div className="cart-icon-container" style={{ position: "relative" }}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="40"
-          height="40"
-          viewBox="0 0 256 256"
-          className="cart-image"
-          aria-label="Cart Icon"
-        >
-          <path d="M6 6h15l-2 10H8L6 6zm3 10a2 2 0 100 4 2 2 0 000-4zm8 0a2 2 0 100 4 2 2 0 000-4z"/>
-        </svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="40"
+    height="40"
+    viewBox="0 0 256 256"
+    className="cart-image"
+    aria-label="Cart Icon"
+  >
+    <path d="M6 6h15l-2 10H8L6 6zm3 10a2 2 0 100 4 2 2 0 000-4zm8 0a2 2 0 100 4 2 2 0 000-4z"/>
+  </svg>
 
-        {/* Restoring the exact `.total_cart_amount` element */}
-<span className="total_cart_amount" style={{ display: "block", visibility: "visible" }}>
-  {totalQuantity || 0}
-</span>
-      </div>
-
-      {/* Ensuring product list renders correctly */}
+  {/* REMOVE the condition so it always renders */}
+  <span className="total_cart_amount">{totalQuantity || 0}</span>
+</div>
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
         <ProductList onHomeClick={handleHomeClick} />
       </div>
